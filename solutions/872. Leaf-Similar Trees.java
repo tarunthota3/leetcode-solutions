a372a31d0a62026f9787ba398f6f2203ead1c0ea
Tarun Thota <tarunthota3@gmail.com>
2020-08-12 09:41:29 -0400

    0 ms

---
+++ solutions/872. Leaf-Similar Trees.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList();
+        List<Integer> list2 = new ArrayList();
+        getLeaf(root1, list1);
+        getLeaf(root2, list2);
+        
+        return list1.equals(list2);
+    }
+    
+    public void getLeaf(TreeNode root, List<Integer> list){
+        if(root != null){
+            if(root.left == null && root.right == null)
+                list.add(root.val);
+            getLeaf(root.left, list);
+            getLeaf(root.right, list);
+        }
+    }
+}
