19f5c1ce56dc0e3df46defa15d66201c20aefae5
Tarun Thota <tarunthota3@gmail.com>
2020-08-12 10:10:44 -0400

    40 ms

---
+++ solutions/73. Set Matrix Zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<Integer> row = new ArrayList();
+        List<Integer> col = new ArrayList();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(row.contains(i) || col.contains(j))
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+}
