c002ea22ed543cc23ab8005a7e4938894b1aeb5e
Tarun Thota <tarunthota3@gmail.com>
2020-08-12 10:06:20 -0400

    2 ms

---
+++ solutions/7. Reverse Integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverse(int x) {
+        String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
+try {
+    return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
+} catch (NumberFormatException e) {
+    return 0;
+}
+    }
+}
