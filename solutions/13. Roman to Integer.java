db9511ec0b9c239aeafd55820e9f62863c518a34
Tarun Thota <tarunthota3@gmail.com>
2021-03-16 08:10:31 -0400

    51 ms

--- solutions/13. Roman to Integer.java
+++ solutions/13. Roman to Integer.java
@@ -1,39 +1,46 @@
 class Solution {
-    int value(char r) 
-    { 
-        int res = 0;
-        if (r == 'I') 
-            res = 1; 
-        if (r == 'V') 
-            res = 5; 
-        if (r == 'X') 
-            res = 10; 
-        if (r == 'L') 
-            res = 50; 
-        if (r == 'C') 
-            res = 100; 
-        if (r == 'D') 
-            res = 500; 
-        if (r == 'M') 
-            res = 1000; 
-        return res;
-    }
     public int romanToInt(String s) {
-        int total = 0;
-        for(int i=0; i<s.length();i++){
+        int res = 0, prev = 0, curr = 0;
+        
+        for(int i = (s.length()-1); i >= 0; i--){
             
-            if (i+1 < s.length()) 
-            {
-                int c1Value = value(s.charAt(i));
-                int c2Value = value(s.charAt(i+1));
-                if(c1Value < c2Value) {
-                    total = total+ c2Value-c1Value;
-                    i++;
-                }
-                else {
-                    total = total + c1Value;
-                }
+            switch(s.charAt(i)){
+                
+                case 'I':
+                    curr = 1;
+                    break;
+                case 'V':
+                    curr = 5;
+                    break;
+                case 'X':
+                    curr = 10;
+                    break;
+                case 'L':
+                    curr = 50;
+                    break;
+                case 'C':
+                    curr = 100;
+                    break;
+                case 'D':
+                    curr = 500;
+                    break;
+                case 'M':
+                    curr = 1000;
+                    break;
+            }
+            if(curr < prev){
+                res -= curr;
+                prev = curr;
             }
             else{
-                total = total + value(s.charAt(i));
+                res += curr;
+                prev = curr;
             }
+            
+            
+        }
+        
+        return res;
+​
+    }
+}
