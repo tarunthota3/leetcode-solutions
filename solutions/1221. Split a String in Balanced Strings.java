e7fb836272b33deb919b8a32f753b446e0bcd536
Tarun Thota <tarunthota3@gmail.com>
2021-03-24 11:34:08 -0400

    0 ms

--- solutions/1221. Split a String in Balanced Strings.java
+++ solutions/1221. Split a String in Balanced Strings.java
@@ -1,12 +1,20 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int count=0;
-        int output=0;
-        for(char c : s.toCharArray()){
-            count += (c == 'L' ? 1 : -1);
-            if(count == 0)
-                output++;
+        int count = 0;
+        int right=0;
+        int left=0;
+        char arr[] = s.toCharArray();
+        for(char i:arr){
+            if(i == 'R') 
+                right++;
+            if(i == 'L') 
+                left++;
+            if(right == left){
+                count++;
+                right=0;
+                left=0;
+            }
         }
-        return output;
+        return count;
     }
 }
