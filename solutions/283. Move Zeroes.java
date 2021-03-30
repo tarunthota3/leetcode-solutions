11897f34916280dd7dc99a0caaff9413c34284bd
Tarun Thota <tarunthota3@gmail.com>
2021-03-30 18:26:52 -0400

    0 ms

--- solutions/283. Move Zeroes.java
+++ solutions/283. Move Zeroes.java
@@ -1,16 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int noOfZeros = 0;
-        for(int j = 0; j < nums.length; j++) {
-            if(nums[j] == 0) {
-                noOfZeros++;
-            }
-            else {
-                nums[j-noOfZeros] = nums[j];
-            }
-        }
-        for(int i = nums.length-1; i > nums.length-1-noOfZeros; i-- ) {
-            nums[i] = 0;
-        }
+        for(int i = 0, j = 0; i < nums.length; i++){
+            if(nums[i] != 0 && nums[j] == 0){
+                nums[j] = nums[i];
+                nums[i] = 0;
+            }
+            while( j < i && nums[j] != 0) j++;
+        }
     }
 }
