38280c812816aed563765b68333dd8d317f7f86c
Tarun Thota <tarunthota3@gmail.com>
2021-03-31 18:32:00 -0400

    2 ms

--- solutions/350. Intersection of Two Arrays II.java
+++ solutions/350. Intersection of Two Arrays II.java
@@ -2,21 +2,30 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        ArrayList<Integer> arrayList = new ArrayList();
-        int k = 0;
-        for(int i = 0; i < nums1.length; i++) {
-            for(int j = k; j < nums2.length; j++) {
-                if(nums1[i] == nums2[j]) {
-                    arrayList.add(nums2[j]);
-                    k = j+1;
-                    break;
-                }
-            }
+        int p1 = 0, p2 = 0, p3 = 0;
+        int l1 = nums1.length, l2 = nums2.length;
+        int[] res = new int[Math.max(l1, l2)] ;
+        while(p1 < l1 && p2 < l2){
+            while(p1 < l1 && nums1[p1] < nums2[p2])
+                p1++;
+            if(p1 == l1)
+                break;
+            while(p2 < l2 && nums1[p1] > nums2[p2])
+                p2++;
+            if(p2 == l2)
+                break;
+            while(p1 < l1 && p2 < l2 && nums1[p1] == nums2[p2]){
+                res[p3] = nums1[p1];
+                p1++;
+                p2++;
+                p3++;
+            }
         }
-        int[] arr = new int[arrayList.size()]; 
-        for(int i =0;i<arr.length; i++) {
-            arr[i] = arrayList.get(i);
+        int[] out = new int[p3];
+        for(int i = 0; i < p3; i++){
+            out[i] = res[i];
         }
-        return arr;
+        return out;
+        
     }
 }
