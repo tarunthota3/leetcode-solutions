d65a15e856977d9ed748a42b773e31ddae3d806e
Tarun Thota <tarunthota3@gmail.com>
2021-03-31 18:36:47 -0400

    0 ms

--- solutions/27. Remove Element.java
+++ solutions/27. Remove Element.java
@@ -1,10 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count = 0;
-        for(int i=0; i<nums.length; i++)
-            if(nums[i] != val)
-                nums[count++] = nums[i];
-        
-        return count;
+       int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
