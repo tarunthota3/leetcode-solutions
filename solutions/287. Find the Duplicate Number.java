e0f2ea5fe820b6ab0e0dcab87b1dd4a657ef6328
Tarun Thota <tarunthota3@gmail.com>
2021-04-01 17:37:55 -0400

    2 ms

--- solutions/287. Find the Duplicate Number.java
+++ solutions/287. Find the Duplicate Number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length-1; i++)
-            if(nums[i] == nums[i + 1])
-                return nums[i];
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num))
+                return num;
+            set.add(num);
+        }
         return -1;
     }
 }
